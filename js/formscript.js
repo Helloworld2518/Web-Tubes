4bbf91cbb00feaf12ab4023fcbbd55c7efd8c51e
Helloworld2518 <ekaadi0906@gmail.com>
2025-12-02 21:23:10 +0700

    FIX form bug

--- js/formscript.js
+++ js/formscript.js
@@ -1,19 +1,21 @@
-function kirim(){
-    var penerima = document.getElementById("footer-penerima").value;
-    var pesan = document.getElementById("footer-pesan").value;
-    var nama = document.getElementById("footer-nama").value;
-    var pesanfinal = "Halo, nama saya: " + nama + "Pesan: " + pesan;
-    var wa_eka= "http://wa.me/+6281901242626?text=";
-    var wa_clay = "http://wa.me/+6285174078350?text=";
-    var wa_daniel = "http://wa.me/+6281380214250?text=";
-    
-    if (penerima == "eka"){
-        window.open(wa_eka + encodeURIComponent(pesanfinal));
+function kirim() {
+  var penerima = document.getElementById("footer-penerima").value;
+  var pesan = document.getElementById("footer-pesan").value;
+  var nama = document.getElementById("footer-nama").value;
+  var pesanfinal = "Halo, nama saya: " + nama + "Pesan: " + pesan;
+  var wa_eka = "http://wa.me/+6281901242626?text=";
+  var wa_clay = "http://wa.me/+6285174078350?text=";
+  var wa_daniel = "http://wa.me/+6281380214250?text=";
+
+  if (pesan == "" || nama == "") {
+    alert("Nama atau pesan tidak boleh kosong");
+  } else {
+    if (penerima == "eka") {
+      window.open(wa_eka + encodeURIComponent(pesanfinal));
+    } else if (penerima == "clay") {
+      window.open(wa_clay + encodeURIComponent(pesanfinal));
+    } else {
+      window.open(wa_daniel + encodeURIComponent(pesanfinal));
     }
-    else if (penerima == "clay"){
-        window.open(wa_clay + encodeURIComponent(pesanfinal));
-    } 
-    else {
-        window.open(wa_daniel + encodeURIComponent(pesanfinal));
-    }
+  }
 }
